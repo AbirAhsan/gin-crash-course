758eafb083236c59a62fcdfe8d3f53dab40a68f7
Abir Ahsan <abirahsan122@gmail.com>
2023-11-10 12:03:55 +0600

    successfully make a dumy post and get request

---
+++ server.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/AbirAhsan/gin-crash-course/controller"
+	"github.com/AbirAhsan/gin-crash-course/service"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	VideoController controller.VideoController = controller.New(videoService)
+)
+
+func main() {
+	server := gin.Default()
+
+	// server.GET("/posts", func(ctx *gin.Context) {
+	// 	ctx.JSON(200, gin.H{
+	// 		"message": "OK!",
+	// 	})
+	// })
+	server.GET("/getVideos", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"success":   true,
+			"videoList": VideoController.FindAll(),
+		})
+
+	})
+	server.POST("/saveVideos", func(ctx *gin.Context) {
+		ctx.JSON(200, VideoController.Save(ctx))
+	})
+
+	server.Run(":8080")
+}
