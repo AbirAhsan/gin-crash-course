b5a93be41bd446322c860ef1ad2789256b123494
Abir Ahsan <abirahsan122@gmail.com>
2023-11-10 23:23:04 +0600

    add tester

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	middlewares "github.com/AbirAhsan/gin-crash-course/middleware"
 	"github.com/AbirAhsan/gin-crash-course/service"
 	"github.com/gin-gonic/gin"
+	gindump "github.com/tpkeeper/gin-dump"
 )
 
 var (
@@ -15,7 +16,7 @@ var (
 func main() {
 	middlewares.SetUpLogOutput()
 	server := gin.New()
-	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth())
+	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth(), gindump.Dump())
 
 	// server.GET("/posts", func(ctx *gin.Context) {
 	// 	ctx.JSON(200, gin.H{
